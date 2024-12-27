45e5f5cd183c5bfb72a9146cced1f6ab53f59979
Keeper477 <serg.prok477@gmail.com>
2024-12-27 16:54:34 +0300

    Init Back

---
+++ back/src/main/java/ru/mirea/network/operational/support/system/back/NetworkOperationalSupportSystemBackApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.network.operational.support.system.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkOperationalSupportSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkOperationalSupportSystemBackApplication.class, args);
+	}
+
+}
