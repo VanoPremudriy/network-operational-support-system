4516c6ab40006ffe2556e8a303cb3609f6541d95
Keeper477 <serg.prok477@gmail.com>
2025-01-12 01:50:07 +0300

    Первая версия бека

--- back/src/main/java/ru/mirea/network/operational/support/system/back/mapper/RootMapper.java
+++ back/src/main/java/ru/mirea/network/operational/support/system/back/mapper/RootMapper.java
@@ -1,12 +1,15 @@
 package ru.mirea.network.operational.support.system.back.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValueCheckStrategy;
 import ru.mirea.network.operational.support.system.api.calculate.route.CalculateRouteRq;
 import ru.mirea.network.operational.support.system.api.calculate.route.CalculateRouteRs;
 import ru.mirea.network.operational.support.system.api.create.route.CreateRouteRq;
 import ru.mirea.network.operational.support.system.api.create.route.CreateRouteRs;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface RootMapper {
     CalculateRouteRq map(CreateRouteRq rq);
 
