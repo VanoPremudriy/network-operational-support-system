cb5244f6d70acd640850155157176561c6890461
Keeper477 <serg.prok477@gmail.com>
2025-02-08 18:24:55 +0300

    Добавил gateway

---
+++ back/network-operational-support-system-gateway/src/main/java/ru/mirea/network/operational/support/system/gateway/NetworkOperationalSupportSystemGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.network.operational.support.system.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkOperationalSupportSystemGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkOperationalSupportSystemGatewayApplication.class, args);
+	}
+
+}
---
+++ back/network-operational-support-system-gateway/src/test/java/ru/mirea/network/operational/support/system/gateway/NetworkOperationalSupportSystemGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.network.operational.support.system.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkOperationalSupportSystemGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
