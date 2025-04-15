494d8a268ccca35eebd07642d2979c579b33a321
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2025-04-15 03:53:50 +0300

    добавление хэдера и сайдбара

--- frontend/src/main/frontend/components/Header/Header.tsx
+++ frontend/src/main/frontend/components/Header/Header.tsx
@@ -1,30 +1,49 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import styles from './Header.module.css'
 import Logo from 'Frontend/components/Header/Logo/Logo';
-import CustomButton from 'Frontend/components/CustomButton/CustomButton';
-import {Users, Box, LineChart, ChartColumn} from "lucide-react"
+import CustomButtonWithIcon from 'Frontend/components/CustomButton/CustomButtonWithIcon';
+import {Users, Box, LineChart} from "lucide-react"
 import { LuChartColumnBig } from "react-icons/lu";
+import { RiSettings4Fill } from "react-icons/ri";
+import IconButton from 'Frontend/components/IconButton/IconButton';
+import { IoIosNotifications } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
 
 const Header =() => {
   return (
     <div className={styles.header}>
       <Logo />
       {menu()}
+      {userMenu()}
     </div>
 )
 
 }
 
 const menu = () => {
+
  return (
    <div className={styles.menu}>
-     <CustomButton label={"Клиенты"} onClick={() => {}} icon={<Users size={20}/>} />
-     <CustomButton label={"Построение маршрута"} onClick={() => {}} icon={<LineChart size={20}/>} />
-     <CustomButton label={"Оборудование"} onClick={() => {}} icon={<Box size={20}/>} />
-     <CustomButton label={"Отчеты"} onClick={() => {}} icon={<LuChartColumnBig size={20}/>}/>
+     <CustomButtonWithIcon label={"Клиенты"} onClick={async () => {}} icon={<Users size={20}/>} />
+     <CustomButtonWithIcon label={"Построение маршрута"} onClick={() => {}} icon={<LineChart size={20}/>} />
+     <CustomButtonWithIcon label={"Оборудование"} onClick={() => {}} icon={<Box size={20}/>} />
+     <CustomButtonWithIcon label={"Отчеты"} onClick={() => {}} icon={<LuChartColumnBig size={20}/>}/>
    </div>
  )
+}
+
+const userMenu = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.userMenu}>
+      <IconButton onClick={() => {
+        localStorage.removeItem("token");
+        navigate("/")
+      }} icon={<RiSettings4Fill size={25}/>}/>
+      <IconButton onClick={() => {}} icon={<IoIosNotifications size={25}/>}/>
+    </div>
+  )
 }
 
 export default Header;
--- frontend/src/main/frontend/components/Sidebar/Sidebar.tsx
+++ frontend/src/main/frontend/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,36 @@
 import styles from 'Frontend/components/Sidebar/Sidebar.module.css';
-import React from 'react';
+import React, { useState } from 'react';
+import SelectButton from 'Frontend/components/SelectButton/SelectButton';
+import CustomButtonColor from 'Frontend/components/CustomButton/CustomButtonColor';
+import CustomButtonNonColor from 'Frontend/components/CustomButton/CustomButtonNonColor';
+
 
 const Sidebar = () => {
-  return (
-    <div className={styles.sidebar}>
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
+  const toggleSidebar = () => {
+    setIsCollapsed(prev => !prev);
+  };
+
+  return (
+    <div className={`${styles.sidebar} ${isCollapsed ? styles.collapsed : ''}`}>
+      <button className={styles.toggleButton} onClick={toggleSidebar}>
+        {isCollapsed ? '⮞' : '⮜'}
+      </button>
+      {!isCollapsed && (
+        <div className={styles.selectBlock}>
+          <SelectButton />
+          <SelectButton />
+          <SelectButton />
+          <div className={styles.buttons}>
+            <CustomButtonColor label={"Построить"} onClick={() => {}} />
+            <CustomButtonNonColor label={"Отменить"} onClick={() => {}} />
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
+
 
 export default Sidebar;
