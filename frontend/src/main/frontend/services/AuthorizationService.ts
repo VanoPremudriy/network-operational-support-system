9227ad2093391d4fa321c87dd70110cfa8e53348
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2025-05-08 17:17:05 +0300

    сервис вызова эндпоинтов и передачи на фронт

--- frontend/src/main/frontend/services/AuthorizationService.ts
+++ frontend/src/main/frontend/services/AuthorizationService.ts
@@ -1,14 +1,52 @@
 import {AuthEndpoint} from "Frontend/generated/endpoints"
 
+export const getAuthToken = async (formData: {
+  login: string;
+  password: string;
+}): Promise<{ success: boolean; errors?: Record<string, string> }> => {
+  const response = await AuthEndpoint.auth(formData);
 
-
-export const getAuthToken = async () => {
-  const response = await AuthEndpoint.auth();
-  if (response.token) {
+  if (response.success && response.token) {
     localStorage.setItem("token", response.token);
-    return true
+    return { success: true };
   } else {
-    console.error("Ошибка: токен отсутствует!");
-    return false;
+    const infos = response.error?.infos || {};
+    const filteredInfos: Record<string, string> = {};
+
+    for (const key in infos) {
+      const value = infos[key];
+      if (typeof value === 'string') {
+        filteredInfos[key] = value;
+      }
+    }
+
+    return { success: false, errors: filteredInfos };
   }
 };
+
+
+export const registration = async (formData: {
+  login: string,
+  password: string,
+  firstName: string,
+  lastName: string
+}):Promise<{ success: boolean; errors?: Record<string, string> }> => {
+  const response = await AuthEndpoint.register(formData);
+
+  if (response.success && response.token) {
+    localStorage.setItem("token", response.token);
+    return { success: true };
+  } else {
+    const infos = response.error?.infos || {};
+    const filteredInfos: Record<string, string> = {};
+
+    for (const key in infos) {
+      const value = infos[key];
+      if (typeof value === 'string') {
+        filteredInfos[key] = value;
+      }
+    }
+
+    return { success: false, errors: filteredInfos };
+  }
+}
