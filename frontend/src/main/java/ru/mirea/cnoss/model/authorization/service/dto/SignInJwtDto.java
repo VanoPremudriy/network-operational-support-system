ec2ef7c5a370c4beac212e802576202cdf3c584d
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2025-05-08 17:14:51 +0300

    изменение дто авторизации

--- frontend/src/main/java/ru/mirea/cnoss/model/authorization/service/dto/SignInJwtDto.java
+++ frontend/src/main/java/ru/mirea/cnoss/model/authorization/service/dto/SignInJwtDto.java
@@ -8,11 +8,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SignInJwtDto {
+    @Nullable
     private String token;
+
     @Schema(description = "Флаг успеха", example = "true")
     private boolean success;
 
     @Schema(description = "Описание ошибок")
     @Nullable
-    private ErrorDto errorDTO;
+    private ErrorDto error;
 }
