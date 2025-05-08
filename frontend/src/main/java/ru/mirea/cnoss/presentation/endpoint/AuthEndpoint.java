dde8e2808a30e2919b22ab4e784822bbeaa5c443
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2025-05-08 17:16:22 +0300

    ендпоинты авторизации и регистрации

--- frontend/src/main/java/ru/mirea/cnoss/presentation/endpoint/AuthEndpoint.java
+++ frontend/src/main/java/ru/mirea/cnoss/presentation/endpoint/AuthEndpoint.java
@@ -4,9 +4,7 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.BrowserCallable;
 import lombok.RequiredArgsConstructor;
 import ru.mirea.cnoss.model.authorization.service.AuthorizationService;
-import ru.mirea.cnoss.model.authorization.service.dto.ErrorDto;
-import ru.mirea.cnoss.model.authorization.service.dto.SignInDto;
-import ru.mirea.cnoss.model.authorization.service.dto.SignInJwtDto;
+import ru.mirea.cnoss.model.authorization.service.dto.*;
 
 @BrowserCallable
 @AnonymousAllowed
@@ -15,19 +13,11 @@ public class AuthEndpoint {
 
     private final AuthorizationService authorizationService;
 
-    //@TODO нормальная авторизация
-    public SignInJwtDto auth() {
+    public SignInJwtDto auth(SignInDto dto) {
+        return authorizationService.auth(dto);
+    }
 
-        SignInJwtDto dto = new SignInJwtDto();
-        dto.setToken("token");
-        dto.setSuccess(true);
-        return dto;
-
-//        SignInDto dto = new SignInDto();
-//        dto.setLogin("admin");
-//        dto.setPassword("admin");
-//        SignInJwtDto auth = authorizationService.auth(dto);
-//        System.out.println(auth.getToken());
-//        return auth;
+    public SignUpJwtDto register(SignUpDto dto) {
+        return authorizationService.register(dto);
     }
 }
