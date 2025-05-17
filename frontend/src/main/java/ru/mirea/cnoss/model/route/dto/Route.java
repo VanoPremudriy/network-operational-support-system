b2fac725cf10be09df39153fbfcaa12ce2871f8b
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2025-05-17 04:05:12 +0300

    изменение дто рута

--- frontend/src/main/java/ru/mirea/cnoss/model/route/dto/Route.java
+++ frontend/src/main/java/ru/mirea/cnoss/model/route/dto/Route.java
@@ -2,12 +2,13 @@ package ru.mirea.cnoss.model.route.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import ru.mirea.cnoss.model.BaseResponse;
 
 import java.util.Set;
 
 @Getter
 @Setter
-public class Route {
+public class Route extends BaseResponse {
     private Set<Node> nodes;
     private Set<Edge> edges;
 }
