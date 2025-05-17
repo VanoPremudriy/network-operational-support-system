31f57eb535b62754e529f4458527f013641a0594
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2025-05-17 04:15:03 +0300

    добавление выбранного узла

--- frontend/src/main/frontend/views/MapView/MapView.tsx
+++ frontend/src/main/frontend/views/MapView/MapView.tsx
@@ -1,12 +1,15 @@
 import Sidebar from 'Frontend/components/Sidebar/Sidebar';
 import styles from 'Frontend/views/MapView/MapView.module.css'
 import RouteMap from 'Frontend/components/Map/RouteMap';
+import { useState } from 'react';
 
 const MapView = () => {
+  const [selectedPointId, setSelectedPointId] = useState<string | null>(null);
+
   return (
     <div className={styles.body}>
-        <Sidebar/>
-        <RouteMap/>
+        <Sidebar selectedPointId={selectedPointId} onClose={() => setSelectedPointId(null)}/>
+        <RouteMap onPointClick={setSelectedPointId}/>
     </div>
   );
 }
