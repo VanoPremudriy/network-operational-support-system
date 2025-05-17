e9bcf5a77a12f784316944e7e81e8b61d2350bf5
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2025-05-17 04:04:14 +0300

    изменение сервиса рута

--- frontend/src/main/java/ru/mirea/cnoss/model/route/RouteService.java
+++ frontend/src/main/java/ru/mirea/cnoss/model/route/RouteService.java
@@ -2,11 +2,12 @@ package ru.mirea.cnoss.model.route;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.mirea.cnoss.model.route.dto.Route;
+import org.springframework.web.bind.annotation.RequestHeader;
+import ru.mirea.cnoss.model.route.dto.RouteResponse;
 
 @FeignClient(url = "http://localhost:8085/network-operational-support-system-api", value = "route")
 public interface RouteService {
 
-    @GetMapping("/route")
-    Route getRoute();
+    @GetMapping(value = "/route")
+    RouteResponse getRoute(@RequestHeader("Authorization") String authorization);
 }
