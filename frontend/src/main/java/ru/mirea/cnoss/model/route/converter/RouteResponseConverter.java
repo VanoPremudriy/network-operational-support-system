1ad2ff0d4b7203919b250b96d150d3a08c941b95
Ivan Tuzov <vanatuzov.2001.ru@mail.ru>
2025-05-17 04:04:30 +0300

    рут респонс, конвертер

---
+++ frontend/src/main/java/ru/mirea/cnoss/model/route/converter/RouteResponseConverter.java
@@ -0,0 +1,34 @@
+package ru.mirea.cnoss.model.route.converter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.mirea.cnoss.model.route.dto.Edge;
+import ru.mirea.cnoss.model.route.dto.Node;
+import ru.mirea.cnoss.model.route.dto.Route;
+import ru.mirea.cnoss.model.route.dto.RouteResponse;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class RouteResponseConverter {
+
+    private final NodeResponseConverter nodeConverter;
+    private final EdgeResponseConverter edgeConverter;
+
+    public Route convert(RouteResponse response) {
+        Route route = new Route();
+
+        route.setSuccess(response.isSuccess());
+        route.setError(response.getError());
+        Set<Node> nodes = response.getNodes().stream().map(nodeConverter::convert).collect(Collectors.toSet());
+        Set<Edge> edges = response.getEdges().stream().map(edgeConverter::convert).collect(Collectors.toSet());
+
+        route.setNodes(nodes);
+        route.setEdges(edges);
+
+        return route;
+
+    }
+}
